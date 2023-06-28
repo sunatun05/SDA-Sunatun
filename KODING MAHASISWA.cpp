0b6b220ac4065aebbbaf1a70635bfddaf596994b
sunatun05 <138006858+sunatun05@users.noreply.github.com>
2023-06-28 21:19:43 +0700

    git init

---
+++ KODING MAHASISWA.cpp
@@ -0,0 +1,74 @@
+#include <iostream>
+#include <string>
+
+
+using namespace std;
+int main(){
+	string name;
+	string prodi;
+	string nim;
+	
+	cout<<"masukkan data berikut :" <<endl<<endl;
+	cout<<"nama :";
+	getline (cin,name);
+	
+	cout<<"prodi :";
+	getline (cin,prodi);
+	
+	cout<<"nim :";
+	getline (cin,nim);
+	
+	cout<<endl;
+	cout<<"DATA MAHASISWA"<<endl;
+	cout<<".............."<<endl;
+	
+	cout<<"nama :" <<name<<endl;
+	cout<<"prodi :"<<prodi<<endl;
+	cout<<"nim : "<<nim<<endl ;
+	
+	
+	
+		cout<<"nama :";
+	getline (cin,name);
+	
+	cout<<"prodi :";
+	getline (cin,prodi);
+	
+	cout<<"nim :";
+	getline (cin,nim);
+	
+	cout<<endl;
+	cout<<"DATA MAHASISWA"<<endl;
+	cout<<".............."<<endl;
+	
+	cout<<"nama :" <<name<<endl;
+	cout<<"prodi :"<<prodi<<endl;
+	cout<<"nim : "<<nim<<endl ;
+	
+	
+		cout<<"nama :";
+	getline (cin,name);
+	
+	cout<<"prodi :";
+	getline (cin,prodi);
+	
+	cout<<"nim :";
+	getline (cin,nim);
+	
+	cout<<endl;
+	cout<<"DATA MAHASISWA"<<endl;
+	cout<<".............."<<endl;
+	
+	cout<<"nama :" <<name<<endl;
+	cout<<"prodi :"<<prodi<<endl;
+	cout<<"nim : "<<nim<<endl ;
+	
+	
+
+	
+	
+	
+
+	
+	
+}
---
+++ sunatun program perkalian 2 bilangan.cpp
@@ -0,0 +1,21 @@
+
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    float bilangan1, bilangan2, hasil;
+
+    cout << "Masukkan bilangan pertama: ";
+    cin >> bilangan1;
+
+    cout << "Masukkan bilangan kedua: ";
+    cin >> bilangan2;
+
+    hasil = bilangan1 * bilangan2;
+
+    cout << "Hasil perkalian: " << hasil << endl;
+
+    return 0;
+}
